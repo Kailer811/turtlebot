88f94bf8a513addaa3a30ec71c9037df98e98484
Kailer <limklethan@gmail.com>
2026-03-10 19:53:24 +0800

    added setup script

---
+++ setup_script/setup.sh
@@ -0,0 +1,42 @@
+$ sudo apt install python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential
+$ sudo apt install ros-humble-hls-lfcd-lds-driver
+$ sudo apt install ros-humble-turtlebot3-msgs
+$ sudo apt install ros-humble-dynamixel-sdk
+$ sudo apt install ros-humble-xacro
+$ sudo apt install libudev-dev
+$ mkdir -p ~/turtlebot3_ws/src && cd ~/turtlebot3_ws/src
+$ git clone -b humble https://github.com/ROBOTIS-GIT/turtlebot3.git
+$ git clone -b humble https://github.com/ROBOTIS-GIT/ld08_driver.git
+$ git clone -b humble https://github.com/ROBOTIS-GIT/coin_d4_driver
+$ cd ~/turtlebot3_ws/src/turtlebot3
+$ rm -r turtlebot3_cartographer turtlebot3_navigation2
+$ cd ~/turtlebot3_ws/
+$ echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+$ source ~/.bashrc
+$ colcon build --symlink-install --parallel-workers 1
+$ echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
+$ source ~/.bashrc
+$ sudo cp `ros2 pkg prefix turtlebot3_bringup`/share/turtlebot3_bringup/script/99-turtlebot3-cdc.rules /etc/udev/rules.d/
+$ sudo udevadm control --reload-rules
+$ sudo udevadm trigger
+$ echo 'export ROS_DOMAIN_ID=37 #TURTLEBOT3' >> ~/.bashrc
+$ source ~/.bashrc
+$ echo 'export LDS_MODEL=LDS-02' >> ~/.bashrc # If you are using LDS-02
+$ source ~/.bashrc
+$ sudo apt update
+$ sudo apt install -y python3-pip git python3-jinja2 \
+libboost-dev libgnutls28-dev openssl libtiff-dev pybind11-dev \
+qtbase5-dev libqt5core5a libqt5widgets5 meson cmake \
+python3-yaml python3-ply \
+libglib2.0-dev libgstreamer-plugins-base1.0-dev
+$ sudo apt install ros-humble-camera-ros
+$ cd ~
+$ git clone -b v0.5.2 https://github.com/raspberrypi/libcamera.git
+$ cd libcamera
+$ meson setup build --buildtype=release -Dpipelines=rpi/vc4,rpi/pisp -Dipas=rpi/vc4,rpi/pisp -Dv4l2=true -Dgstreamer=enabled -Dtest=false -Dlc-compliance=disabled -Dcam=disabled -Dqcam=disabled -Ddocumentation=disabled -Dpycamera=enabled
+$ ninja -C build -j 1
+$ sudo ninja -C build install -j 1
+$ sudo ldconfig
+$ export LD_LIBRARY_PATH=/usr/local/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
+$ echo "check rpi camera works, setup ssh S1"
+
